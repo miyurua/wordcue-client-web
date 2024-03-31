feff5fcaa88f331c052dec9f5d263be6cd3affe9
miyuru <miyuruabeysiriwardana13@gmail.com>
2024-03-31 13:22:08 +0530

    adding routes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,33 @@
-// import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const Home = () => {
+const Home: React.FC = () => {
+  const [sentence, setSentence] = useState("");
+
+  const [data, setData] = useState([{}]);
+
+  const handleSubmit = async () => {
+    const response = await fetch("/analyze", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ sentence }),
+    });
+    const data = await response.json();
+    console.log(data); // Handle or display the response data as needed
+  };
+
+  useEffect(() => {
+    fetch("/sample")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+
+  console.log(data);
+
   return (
     <div className="flex flex-col md:flex-row divide-y md:divide-y-0 md:divide-x divide-[#008080] h-screen">
       <div className="flex items-center justify-center md:w-1/2 p-4">
@@ -18,10 +45,15 @@ const Home = () => {
           <textarea
             className="form-textarea mt-1 block w-full h-96 resize-none rounded-lg p-4 border border-[#008080] shadow-sm focus:border-green-400 focus:green-400"
             placeholder="Enter your sentence here..."
+            value={sentence}
+            onChange={(e) => setSentence(e.target.value)}
           />
         </div>
         <div className="flex justify-end">
-          <button className="rounded-full bg-gray-800 text-white px-5 py-2 hover:bg-gray-600">
+          <button
+            className="rounded-full bg-gray-800 text-white px-5 py-2 hover:bg-gray-600"
+            onClick={handleSubmit}
+          >
             Submit
           </button>
         </div>
